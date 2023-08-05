6d88bc48fd112a2c5ddbe77015e827bf822166cc
kevin-ledez <kevin.ledez@gmail.com>
2023-08-05 23:20:59 +0200

    🚀 RELEASE: New base Angular

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./Pages/Partials/navbar/navbar.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular';
