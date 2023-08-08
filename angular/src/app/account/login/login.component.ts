96b720e918ce085d749dca2990a4f76ae4192793
kevin-ledez <kevin.ledez@gmail.com>
2023-08-08 11:18:50 +0200

    🐛 FIX: White Page

--- angular/src/app/account/login/login.component.ts
+++ angular/src/app/account/login/login.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '@app/_services';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+  error?: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService
+  ) {
+    if (this.accountService.userValue) {
+      this.router.navigate(['/']);
+    }
+
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    this.error = '';
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.accountService.login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          this.router.navigateByUrl(returnUrl);
+        },
+        error: error => {
+          this.error = error;
+          this.loading = false;
+        }
+      });
+
+  }
 
 }
--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home';
 import { LoginComponent, RegisterComponent } from './account';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: '/account/login', component: LoginComponent },
-    { path: '/account/register', component: RegisterComponent },
+    { path: 'account/login', component: LoginComponent },
+    { path: 'account/register', component: RegisterComponent },
 
     { path: '**', redirectTo: '' }
 ];
