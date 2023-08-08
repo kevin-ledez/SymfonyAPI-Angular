8b29886592583ecb4f1af893254b62f4a76c2c18
kevin-ledez <kevin.ledez@gmail.com>
2023-08-08 23:43:13 +0200

    👌 IMPROVE: Message Error

--- angular/src/app/account/login/login.component.ts
+++ angular/src/app/account/login/login.component.ts
@@ -3,70 +3,61 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
-import { AccountService } from '@app/_services';
+import { AccountService, AlertService } from '@app/_services'
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
+@Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
-  form!: FormGroup;
-  loading = false;
-  submitted = false;
-  error?: string;
-  success?: string;
+    form!: FormGroup;
+    loading = false;
+    submitted = false;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private accountService: AccountService
-  ) {
-    if (this.accountService.userValue) {
-      this.router.navigate(['/']);
-    }
-
-  }
-
-  ngOnInit() {
-    this.form = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
-    if (this.route.snapshot.queryParams.registered) {
-      this.success = 'Registration successful !';
-    }
-
-  }
-
-  get f() { return this.form.controls; }
-
-  onSubmit() {
-    this.submitted = true;
-
-    this.error = '';
-    this.success = '';
-
-    if (this.form.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    this.accountService.login(this.f.username.value, this.f.password.value)
-      .pipe(first())
-      .subscribe({
-        next: () => {
-          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-          this.router.navigateByUrl(returnUrl);
-        },
-        error: error => {
-          this.error = error;
-          this.loading = false;
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) {
+        // redirect to home if already logged in
+        if (this.accountService.userValue) {
+            this.router.navigate(['/']);
         }
-      });
+    }
 
-  }
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    // get return url from query parameters or default to home page
+                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    this.router.navigateByUrl(returnUrl);
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });
+    }
 }
--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { User } from '@app/_models';
+import { AccountService } from '@app/_services';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  user: User | null;
+
+  constructor(private accountService: AccountService) {
+    this.user = this.accountService.userValue;
+  }
 
 }
